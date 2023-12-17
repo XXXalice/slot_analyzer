efcacbcbabe99147cdd100e5d7ffb8d74d600a9d
梅宮 <euthanasia045@gmail.com>
2023-12-18 02:48:15 +0900

    IFの形を決めた

--- analyze.py
+++ analyze.py
@@ -19,10 +19,27 @@ class Analyzer:
         print("データのロードに成功しました")
         return rows
 
+    # JSON風dict作成
+    # 例
+    # {
+    #     "アイムジャグラーEX_116": {
+    #         "2022-09-06": 102,
+    #         "2022-09-07": 99
+    #     },
+    #     "アイムジャグラーEX_117": {
+    #         "2022-09-06": 80,
+    #         "2022-09-07": 95
+    #     }
+    # }
     def total(self, rows):
         data_dict = {}
         for row in rows:
             items = row.split(",")
+            # タイトル
+            slot_title = items[1]
+            # 台番号
+            slot_id = items[2]
+            slot_name = f"{slot_title}_{slot_id}"
 
 
 
@@ -30,6 +47,7 @@ class Analyzer:
         with open(path, mode="r") as f:
             rows = [row.rstrip() for row in f.readlines() if row.split(",")[1] != "閉鎖"][1:]
         return rows
+
 
     def _print_about(self):
         print("スロットアナライザー")
