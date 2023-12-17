edacc263ab3fd32126e00f44d4fb3748e299e523
梅宮 <euthanasia045@gmail.com>
2023-12-18 03:13:44 +0900

    実装続き

--- analyze.py
+++ analyze.py
@@ -1,10 +1,11 @@
 from tqdm import tqdm
 import os
+import itertools
 
 class Analyzer:
-    def __init__(self):
+    def __init__(self, dir_path="files"):
         self.version = "0.1.0"
-        self.dir_name = "files"
+        self.dir_name = dir_path
         self._print_about()
 
 
@@ -17,7 +18,7 @@ class Analyzer:
             path = f"./{self.dir_name}/{fname}"
             rows.append(self._fetch_from_file(path))
         print("データのロードに成功しました")
-        return rows
+        return list(itertools.chain.from_iterable(rows))
 
     # JSON風dict作成
     # 例
@@ -31,7 +32,7 @@ class Analyzer:
     #         "2022-09-07": 95
     #     }
     # }
-    def total(self, rows):
+    def get_value_ratio_at_machine(self, rows):
         data_dict = {}
         for row in rows:
             items = row.split(",")
@@ -40,7 +41,19 @@ class Analyzer:
             # 台番号
             slot_id = items[2]
             slot_name = f"{slot_title}_{slot_id}"
-
+            # 日付
+            timestamp = items[0]
+            # g数
+            game_num = int(items[3])
+            if game_num == 0:
+                continue
+            # 差枚数
+            diff = int(items[4])
+            # 機械割  (G数3)+(差枚数))/(G数3)
+            ratio = (game_num * 3 + diff) / game_num * 3
+            if data_dict
+            data_dict[slot_name][timestamp] = ratio
+        return data_dict
 
 
     def _fetch_from_file(self, path):
@@ -57,7 +70,8 @@ class Analyzer:
 def main():
     analyzer = Analyzer()
     rows = analyzer.collect_datas()
-
+    data_dict = analyzer.get_value_ratio_at_machine(rows)
+    print(data_dict)
 
 
 if __name__ == '__main__':
